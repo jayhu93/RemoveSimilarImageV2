fe0f29765199e143857b1877bc5e333d9fc4e635
Jay Yupin Hu <jay.yupinhu@gmail.com>
2019-06-08 09:52:56 -0700

    Permission fix

--- Clear/Service/PhotoLibraryService.swift
+++ Clear/Service/PhotoLibraryService.swift
@@ -109,23 +109,13 @@ final class PhotoLibraryService: NSObject, PHPhotoLibraryChangeObserver, PhotoLi
             // if similar photo still process preview batch, then cancel the request
                 guard let strongSelf = self else { return }
 
-                let begin = currentCount - 1
+                let begin = currentCount
                 let possibleEnd = begin + Constants.pageSize
 
                 let allPhotoOptions = PHFetchOptions()
                 allPhotoOptions.sortDescriptors = [NSSortDescriptor(key: "creationDate", ascending: false)]
                 allPhotoOptions.fetchLimit = possibleEnd
                 let fetch = PHAsset.fetchAssets(with: allPhotoOptions)
-
-                guard fetch.count > 0 else { return }
-
-//                let begin = currentCount - 1
-//                let possibleEnd = begin + Constants.pageSize
-//                let lastIndex = fetch.count - 1
-//                let end = lastIndex > possibleEnd ? possibleEnd : lastIndex
-//                let indexSet = IndexSet(currentCount...end)
-
-//                let assets = fetch.objects(at: indexSet)
 
                 strongSelf.fetchResultProperty.value = fetch
         }
@@ -162,12 +152,41 @@ final class PhotoLibraryService: NSObject, PHPhotoLibraryChangeObserver, PhotoLi
     // MARK: PHPhotoLibraryChangeObserver
 
     func photoLibraryDidChange(_ changeInstance: PHChange) {
+        print("print: photo library did change")
         // TODO: Do something here
-//        guard let changes = changeInstance.changeDetails(for: fetchResultProperty.value ?? PHFetchResult<PHAsset>())
-//            else { return }
-//        DispatchQueue.main.sync {
-//            fetchResultProperty.value = changes.fetchResultAfterChanges
-//        }
+        guard let changes = changeInstance.changeDetails(for: fetchResultProperty.value ?? PHFetchResult<PHAsset>())
+            else { return }
+        DispatchQueue.main.sync {
+            // Hang on to the new fetch result.
+            print("print: set fetch result")
+            fetchResultProperty.value = changes.fetchResultAfterChanges
+            // If we have incremental changes, animate them in the collection view.
+//            if changes.hasIncrementalChanges {
+//                guard let collectionView = self.collectionView else { fatalError() }
+//                // Handle removals, insertions, and moves in a batch update.
+//                collectionView.performBatchUpdates({
+//                    if let removed = changes.removedIndexes, !removed.isEmpty {
+//                        collectionView.deleteItems(at: removed.map({ IndexPath(item: $0, section: 0) }))
+//                    }
+//                    if let inserted = changes.insertedIndexes, !inserted.isEmpty {
+//                        collectionView.insertItems(at: inserted.map({ IndexPath(item: $0, section: 0) }))
+//                    }
+//                    changes.enumerateMoves { fromIndex, toIndex in
+//                        collectionView.moveItem(at: IndexPath(item: fromIndex, section: 0),
+//                                                to: IndexPath(item: toIndex, section: 0))
+//                    }
+//                })
+//                // We are reloading items after the batch update since `PHFetchResultChangeDetails.changedIndexes` refers to
+//                // items in the *after* state and not the *before* state as expected by `performBatchUpdates(_:completion:)`.
+//                if let changed = changes.changedIndexes, !changed.isEmpty {
+//                    collectionView.reloadItems(at: changed.map({ IndexPath(item: $0, section: 0) }))
+//                }
+//            } else {
+//                // Reload the collection view if incremental changes are not available.
+//                collectionView.reloadData()
+//            }
+//            resetCachedAssets()
+        }
     }
 
     // MARK: PhotoLibraryServiceType
